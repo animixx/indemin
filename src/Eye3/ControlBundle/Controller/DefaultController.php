8f0631d3c72a9aeb5e02582c2c230626e1492537
animix <animix@vtr.net>
2014-09-09 17:17:37 -0600

    corrigiendo logout

--- src/Eye3/ControlBundle/Controller/DefaultController.php
+++ src/Eye3/ControlBundle/Controller/DefaultController.php
@@ -3,12 +3,14 @@
 namespace Eye3\ControlBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-		return $this->redirect($this->generateUrl('eye3_control_estadisticas_tiempo'));
+		// return $this->redirect($this->generateUrl('eye3_control_estadisticas_tiempo'));
+		return $this->render('Eye3ControlBundle:Default:index.html.twig');
     }
 }
